1c918d87adb7abf6043b18e58768ec7a6325ce43
Pranav Arora <aurorapranav187@gmail.com>
2021-11-26 13:35:52 +0530

    DEBUG

--- tubers/settings.py
+++ tubers/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v8^@c)l%obw$t_9+n7uu_9!-$$rghf_l#i8w0lku9w&(cpqz=0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['hireourtubers.herokuapp.com']
 
